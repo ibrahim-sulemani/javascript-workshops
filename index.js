5ea737a5e35d5f3d8de748255379b865893c390b
Ibrahim Sulemani <sulemani.ibrahim@gmail.com>
2024-05-25 01:10:59 +0200

    solution of workshop-1/exercise-2

---
+++ index.js
@@ -0,0 +1,23 @@
+
+/*
+Checking Day of the Week
+- Monday - Start of the work week!
+- Wednesday - Midweek!
+- Friday - Almost weekend!
+- Other day - Just another day.
+*/
+
+let today = "Monday";
+
+if (today === "Monday") {
+    console.log("Start of the work week!");
+} else if (today === "Wednesday") {
+    console.log("Midweek!");
+} else if (today === "Friday") {
+    console.log("Almost weekend!");
+} else {
+    console.log("Just another day.");
+}
+
+
+
