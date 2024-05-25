df53d0dea94d53f01f9a78d1a14c8d4c060deeca
Ibrahim Sulemani <sulemani.ibrahim@gmail.com>
2024-05-25 16:18:39 +0200

    solution of workshop-2/exercises-1

---
+++ index.js
@@ -0,0 +1,9 @@
+/*  1. Kreirajte varijabla koja ke go cuva e-mailot od korisnik. 
+    Napravete proverka dokolku ima prazni mesta na pocetok ili
+    na kraj da gi izvadite istite i istiot sekogas da e so mali bukvi,
+    bez razlika kako ke go vnese korisnikot.  
+  */
+
+    let email = "   Ibrahim.Sulemani@gmail.com    ";
+
+    console.log(email.trim().toLowerCase());
