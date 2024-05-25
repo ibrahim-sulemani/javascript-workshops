9ffc0eddca31dbd705d7c3007643a9cf83b6d072
Ibrahim Sulemani <sulemani.ibrahim@gmail.com>
2024-05-25 12:27:49 +0200

    solution workshop-1/ternary-2

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// 2. Determining Pass or Fail Based on Score 0-100 ( below 65 is a fail )
+
+let score = 70;
+
+if (score < 65) {
+    console.log("Fail");
+} else {
+    console.log("Pass");
+};
+
+score < 65 ? console.log("Fail") : console.log("Pass");
+
