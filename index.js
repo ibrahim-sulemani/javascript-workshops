b8b65cad75042bdd9896e6bfe9bf5c7902a56adb
Ibrahim Sulemani <sulemani.ibrahim@gmail.com>
2024-05-25 12:16:00 +0200

    solution workshop-1/ternary operator -1

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// 1. Check Age for Voting Eligibility
+
+let age = 25;
+
+if (age >= 18) {
+    console.log('You are eligible to vote');
+} else {
+    console.log('You are not eligible to vote');
+}
+
+age >= 18 ? console.log('You are eligible to vote') : console.log('You are not eligible to vote');
