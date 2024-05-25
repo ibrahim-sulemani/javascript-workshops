6956a644e7b81ffa41772b16bcd4a2bafaa2ff20
Ibrahim Sulemani <sulemani.ibrahim@gmail.com>
2024-05-25 16:40:11 +0200

    solution of workshop-2/exercise-4

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+4. Vo recenicata "I love cats, cats are awesome" promenete gi cats so dogs. 
+
+*/
+
+let text = "I love cats, cats are awesome";
+
+let newText = text.replaceAll("cats", "dogs");
+console.log(newText);
