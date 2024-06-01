18febd875f7fbe46e55c230ccc7df7df1e53352b
Ibrahim Sulemani <sulemani.ibrahim@gmail.com>
2024-06-01 17:31:21 +0200

    solution of workshop-3/exercise-9

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+- Funkcija koja sto koga i da ja povikame ke ni dade random greeting od ponudenata niza: 
+const greetings = ["Hello!", "Hi!", "Hey there!", "Greetings!", "Salutations!"];
+*/
+
+function greeting() {
+    const greetings = ["Hello!", "Hi!", "Hey there!", "Greetings!", "Salutations!"];
+
+    let index = Math.floor(Math.random() * 5);
+
+    return greetings[index];
+}
+
+console.log(greeting());
